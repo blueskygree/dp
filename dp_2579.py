941e25b218ed56daadf5435cf26d332be017294d
blueskygree <133464320+blueskygree@users.noreply.github.com>
2024-11-04 20:33:47 +0900

    Update dp_2579.py

--- dp_2579.py
+++ dp_2579.py
@@ -10,9 +10,9 @@ for i in range(n):
 
 d[0]=data[0]
 d[1]=data[0]+data[1]
-d[2]=max(data[0]+data[2],d[1],data[1]+data[2])
+d[2]=max(data[0]+data[2],data[1]+data[2]) # d[2]=max(data[0]+data[2],d[1],data[1]+data[2]) 아닌 이유:
 
 for i in range(3,n):
-    d[i]=max(d[i-2]+data[i],data[i-1]+data[i]+d[i-3])
+    d[i]=max(d[i-2]+data[i],data[i-1]+data[i]+d[i-3]) # d[i]를 밟고 있다는 가정이기 때문에 d[1]이 들어가면 연속 3개를 밟는것이다.
 
 print(d[n-1])
